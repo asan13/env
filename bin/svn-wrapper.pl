dbc51f3ccd60bdbc8a103bd47fd800d6476a32e5
Andrey Asyakin <asan999@gmail.com>
2016-01-19 02:39:54 +0300

    parse arg for search commit files

--- bin/svn-wrapper.pl
+++ bin/svn-wrapper.pl
@@ -4,6 +4,7 @@ use warnings;
 use feature 'say';
 use Carp;
 use Cwd;
+use Getopt::Long;
 
 BEGIN {
     my $SVN;
@@ -18,20 +19,20 @@ BEGIN {
 exec_svn unless @ARGV;
 
 
-my $action = '';
-for ( @ARGV ) {
-    /^\b([a-z]+)\b$/ && ($action = $1, last);
-}
-exec_svn unless $action;
-
 my $hooks_dir = "$ENV{HOME}/.subversion/hooks";
 exec_svn unless -d $hooks_dir;
 
-my $rep_dir = Cwd::abs_path -e $ARGV[-1] ? $ARGV[-1] : undef;
-exec_svn unless $rep_dir;
-
+my $action = $ARGV[0];
 if ($action =~ /^(?:commit|ci)$/ && -x "$hooks_dir/client-pre-commit") {
-    my $st = system("$hooks_dir/client-pre-commit", $rep_dir, @ARGV);
+    my $argv = eval { parse_argv(@ARGV[1..@ARGV-1]) };
+    exec_svn if $@;
+
+    my @files = @{ delete $argv->{_files} };
+    my @hook_args;
+    if ($argv->{'config-dir'}) {
+        push @hook_args, '--config-dir' => $argv->{'config-dir'};
+    }
+    my $st = system("$hooks_dir/client-pre-commit", @hook_args, @files);
     if ($st != 0) {
         say 'client-pre-commit return error';
         exit;
@@ -40,3 +41,51 @@ if ($action =~ /^(?:commit|ci)$/ && -x "$hooks_dir/client-pre-commit") {
 
 exec_svn;
 
+sub parse_argv {
+    my @argv = @_;
+    return unless @argv;
+    my @opts;
+    while (<DATA>) {
+        s/#.*$//;
+        next if /^\s*$/;
+        my $opt = join '|', /(?:-+([^\s]+))+/g;
+        $opt .= '=s' if /ARG/;
+        push @opts, $opt;
+    }
+    my %flush;
+    Getopt::Long::GetOptionsFromArray(\@argv, \%flush, @opts) or die 42;
+    if ($flush{targets}) {
+        if ( open my $fh, '<', $flush{targets} ) {
+            while ($fh) {
+                push @argv, $_;
+            }
+            close $fh;
+        }
+    }
+    $flush{_files} = \@argv;
+    return \%flush;
+}
+
+
+
+__DATA__
+  -q --quiet               # print nothing, or only summary information
+  -N --non-recursive       # obsolete; try --depth=files or --depth=immediates
+  --depth ARG              # limit operation by depth ARG ('empty', 'files',
+  --targets ARG            # pass contents of file ARG as additional args
+  --no-unlock              # don't unlock the targets
+  -m --message ARG         # specify log message ARG
+  -F --file ARG            # read log message from file ARG
+  --force-log              # force validity of log message source
+  --editor-cmd ARG         # use ARG as external editor
+  --encoding ARG           # treat value as being in charset encoding ARG
+  --with-revprop ARG       # set revision property ARG in new revision
+  --changelist --cl ARG    # operate only on members of changelist ARG
+  --keep-changelists       # don't delete changelists after commit
+  --username ARG           # specify a username ARG
+  --password ARG           # specify a password ARG
+  --no-auth-cache          # do not cache authentication tokens
+  --non-interactive        # do no interactive prompting
+  --trust-server-cert      # accept SSL server certificates from unknown
+  --config-dir ARG         # read user configuration files from directory ARG
+  --config-option ARG      # set user configuration option in the format:
